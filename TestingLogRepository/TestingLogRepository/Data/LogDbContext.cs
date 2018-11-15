428f422ea96db5154d5cabc3984d2ec24a5e6116
Greg Doud <gpdoud@gmail.com>
2018-11-15 18:12:54 -0500

    "added vs project to repo"

---
+++ TestingLogRepository/TestingLogRepository/Data/LogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestingLogRepository.Models
+{
+    public class LogDbContext : DbContext
+    {
+        public LogDbContext (DbContextOptions<LogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestingLogRepository.Models.Log> Log { get; set; }
+    }
+}
---
+++ TestingLogRepository/TestingLogRepository/Models/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingLogRepository.Models {
+    public class Log {
+
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Message { get; set; }
+
+        public Log() { }
+    }
+}
